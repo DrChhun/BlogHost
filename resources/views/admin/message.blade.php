a7965e97673ed29fccca761ca1bf5a2b61942e79
chhunkim394 <chhunkim394@gmail.com>
2022-11-30 20:41:35 +0700

    message done

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -7,10 +7,13 @@
 @section('content')
     <div class="p-8">
         @foreach($collect as $collect)
-            <h1>{{$collect->email}}</h1>
-            <p>{{$collect->message}}</p>
-
-            <a href="/delete/message/{{$collect->id}}">Delete</a>
+        <div class="flex justify-between py-4 border-b-[1px]">
+            <div>
+                <h1>From : {{$collect->email}} <span class="text-xs ml-4">{{$collect->created_at->diffForHumans()}}</span></h1>
+                <p>> {{$collect->message}}</p>
+            </div>
+            <a class="text-red-600" href="/delete/message/{{$collect->id}}">Delete</a>
+        </div>
         @endforeach
     </div>
 @endsection
